a6a41022cbf59cb54d8d40e03fc4a197fda2e9eb
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2019-09-12 14:01:54 +0900

    자식 Piece들의 수 계산 방식 개선

--- app/src/main/java/com/example/im_yong/Piece.java
+++ app/src/main/java/com/example/im_yong/Piece.java
@@ -6,26 +6,28 @@ public class Piece {
     String title;
     ArrayList<Piece> sub_pieces = new ArrayList<Piece>();
     String memo;
+    int children_count;
 
     Piece(String title, String memo) {
         this.title = title;
         this.memo = memo;
+        this.children_count = 0;
     }
 
     Piece(String title) {
-        this.title = title;
-        this.memo = null;
+        this(title, null);
     }
 
     void put(Piece piece) {
         this.sub_pieces.add(piece);
+        this.children_count++;
     }
 
-    int children_count() {
-        int count = 0;
+    int children_counting() {
+        int count = this.children_count;
         for (Piece piece : sub_pieces) {
-            count += piece.children_count();
+            count += piece.children_counting();
         }
-        return count += sub_pieces.size();
+        return count;
     }
 }
