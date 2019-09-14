a02378a7d4fd271b152170c7d7ebf980ad37e647
kitty-700 <31284335+kitty-700@users.noreply.github.com>
2019-09-14 21:16:43 +0900

    분류 기준 추가 개선 (변수명)

--- app/src/main/java/com/example/im_yong/Piece.java
+++ app/src/main/java/com/example/im_yong/Piece.java
@@ -7,15 +7,15 @@ public class Piece {
     ArrayList<Piece> sub_pieces = new ArrayList<Piece>();
     String memo;
     int children_count;
-    Integer importance; //중요한 정도 0,,,,,,10 or null
-    Integer alone; //단일 용어로 쓰이는지 0(X), 1(O) or null
+    Integer itn; //importance. 중요한 정도 0,,,,,,10 or null
+    Integer al; //alone. 단일 용어로 쓰이는지 1(O) or null
 
-    Piece(String title, Integer alone, Integer importance, String memo) {
+    Piece(String title, Integer al, Integer itn, String memo) {
         this.title = title;
         this.memo = memo;
         this.children_count = 0;
-        this.alone = alone;
-        this.importance = importance;
+        this.al = al;
+        this.itn = itn;
     }
 
     Piece(String title) {
@@ -26,16 +26,16 @@ public class Piece {
         this(title, null, null, memo);
     }
 
-    Piece(String title, Integer alone) {
-        this(title, alone, null, null);
+    Piece(String title, Integer al) {
+        this(title, al, null, null);
     }
 
-    Piece(String title, Integer alone, Integer importance) {
-        this(title, alone, importance, null);
+    Piece(String title, Integer al, Integer itn) {
+        this(title, al, itn, null);
     }
 
-    Piece(String title, Integer alone, String memo) {
-        this(title, alone, null, memo);
+    Piece(String title, Integer al, String memo) {
+        this(title, al, null, memo);
     }
 
     void put(Piece piece) {
